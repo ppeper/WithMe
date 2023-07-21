7c654b257e49558bf26e3b9dbbc6027bdeabdaa2
정진욱 <wlsdnr214@naver.com>
2023-07-21 14:18:32 +0900

    init: spring boot

---
+++ backend/src/main/java/com/ssafy/bonobono/BonobonoApplication.java
@@ -0,0 +1,12 @@
+package com.ssafy.bonobono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonobonoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BonobonoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ssafy/bonobono/BonobonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.bonobono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonobonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

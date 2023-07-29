64aa5153fde983cdd82f6b479d69a6891bee02bc
Juyong <juyong4190@daum.net>
2023-07-29 16:52:31 +0900

    feat: Bottom Navigation 구현

---
+++ android/presentation/src/main/java/com/bonobono/presentation/ui/main/HomeScreen.kt
@@ -0,0 +1,10 @@
+package com.bonobono.presentation.ui.main
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun MainHomeScreen(navController: NavController) {
+    Text(text = "home")
+}
---
+++ android/presentation/src/main/java/com/bonobono/presentation/ui/mypage/MyPageScreen.kt
@@ -0,0 +1,10 @@
+package com.bonobono.presentation.ui.mypage
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun MainMyPageScreen(navController: NavController) {
+    Text(text = "mypage")
+}

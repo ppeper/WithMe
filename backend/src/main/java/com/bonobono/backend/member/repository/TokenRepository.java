791922d08d6c41d8622ea1a5da26d0a11367bfb7
sm9906 <suminkim0618@gmail.com>
2023-08-10 20:09:01 +0900

    feat: AuthorityRepository, TokenRepository 살짝 수정 / MemberController Operation 살짝 수정

--- backend/src/main/java/com/bonobono/backend/member/repository/TokenRepository.java
+++ backend/src/main/java/com/bonobono/backend/member/repository/TokenRepository.java
@@ -3,7 +3,9 @@ package com.bonobono.backend.member.repository;
 import com.bonobono.backend.member.domain.Token;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TokenRepository extends JpaRepository<Token, String> {
 
     Optional<Token> findByKey(String key);

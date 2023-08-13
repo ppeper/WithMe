a1a86093675dae92f20f83ee79c59671d366db75
정진욱 <wlsdnr214@naver.com>
2023-08-13 17:55:54 +0900

    fix: swagger https 인식불가 버그 해결

--- backend/src/test/java/com/bonobono/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/bonobono/backend/BackendApplicationTests.java
@@ -1,8 +1,13 @@
 package com.bonobono.backend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 
 @SpringBootTest
 @WebAppConfiguration

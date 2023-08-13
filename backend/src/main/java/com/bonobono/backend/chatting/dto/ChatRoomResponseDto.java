ef88f190473223cde9f8513b837603d64454fe8a
surimjeon <gds0813@naver.com>
2023-08-13 17:02:34 +0900

    fix: 채팅dto 내 프로필이미지 수정

--- backend/src/main/java/com/bonobono/backend/chatting/dto/ChatRoomResponseDto.java
+++ backend/src/main/java/com/bonobono/backend/chatting/dto/ChatRoomResponseDto.java
@@ -2,6 +2,7 @@ package com.bonobono.backend.chatting.dto;
 
 import com.bonobono.backend.chatting.domain.ChatRoom;
 import com.bonobono.backend.member.domain.Member;
+import com.bonobono.backend.member.domain.ProfileImg;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,13 +17,12 @@ public class ChatRoomResponseDto {
     private int roomNumber;
     private String nickname;
     private String createdDate;
-//    private String updatedDate;
-//    private List<ChatMessageResponseDto> messages;
-
+    private ProfileImg profileImg;
 
     public ChatRoomResponseDto(ChatRoom chatRoom) {
         this.roomNumber = chatRoom.getRoomNumber();
         this.nickname = chatRoom.getOther().getNickname();
+        this.profileImg=chatRoom.getOther().getProfileImg();
         this.createdDate = chatRoom.getCreatedDate().format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
 //        this.updatedDate = chatRoom.getModifiedDate().format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
 //        this.messages = chatRoom.getChatMessageList().stream().map(ChatMessageResponseDto::new).collect(Collectors.toList());
--- backend/src/main/java/com/bonobono/backend/chatting/dto/ChatRoomWithMessagesDto.java
+++ backend/src/main/java/com/bonobono/backend/chatting/dto/ChatRoomWithMessagesDto.java
@@ -3,6 +3,7 @@ package com.bonobono.backend.chatting.dto;
 import com.bonobono.backend.chatting.domain.ChatMessage;
 import com.bonobono.backend.chatting.domain.ChatRoom;
 import com.bonobono.backend.member.domain.Member;
+import com.bonobono.backend.member.domain.ProfileImg;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,10 +14,12 @@ import java.util.List;
 public class ChatRoomWithMessagesDto {
     private String nickname;
     private int roomNumber;
+    private ProfileImg profileImg;
     private List<ChatMessageResponseDto> messages;
 
     public ChatRoomWithMessagesDto(ChatRoom chatRoom, List<ChatMessageResponseDto> messages) {
         this.nickname = chatRoom.getOther().getNickname();
+        this.profileImg= chatRoom.getOther().getProfileImg();
         this.roomNumber = chatRoom.getRoomNumber();
         this.messages = messages;
     }

2c7442e2da2683cc88b5b1b957cff21790fb8700
정진욱 <wlsdnr214@naver.com>
2023-08-14 17:45:51 +0900

    fix: Time_zone UTC -> Asia/Seoul

--- backend/src/main/java/com/bonobono/backend/BackendApplication.java
+++ backend/src/main/java/com/bonobono/backend/BackendApplication.java
@@ -5,6 +5,9 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @OpenAPIDefinition(servers = {@Server(url="https://i9d105.p.ssafy.io", description = "default server url")})
 // aws가 아닌 로컬 환경에서 스프링부트 실행시 발생하는 에러 해결
 @SpringBootApplication(exclude = {
@@ -13,6 +16,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 		io.awspring.cloud.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
 })
 public class BackendApplication {
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
